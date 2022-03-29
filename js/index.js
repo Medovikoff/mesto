23b50862c426ba91c7a1cf29665141528538e855
Medovikov <art-medovikov@yandex.ru>
2022-03-29 11:05:36 +0300

    new card

--- js/index.js
+++ js/index.js
@@ -40,12 +40,37 @@ form.addEventListener('submit', addUser);
 
 //adding places
 
-const popOpenPlace = document.querySelector('.profile__add-button')
+const popOpenPlace = document.querySelector('.profile__add-button');
 
-openPopPlace.addEventListener('click', function(e){
+popOpenPlace.addEventListener('click', function(e){
     e.preventDefault();
-    popUp.classList.add('pop-up_opened');
-    userName.value = profileName.textContent;
-    description.value = profileDiscription.textContent;
-    
-})
+    popUp.classList.add('pop-up_opened')
+});
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
